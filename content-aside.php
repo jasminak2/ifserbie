5d179c945a30489e3c1315ff74dd9cd05404153b
Jasmina <jasmina.konstantinovic@institutfrancais.rs>
2017-07-07 10:44:50 +0200

    Izmenjen content-aside Info post tako da se naslovi prikazuju samo u excerpt-u, a ne i ako nema excerpt. U singlu se i dalje prikazuju. To je uradjeno da bi tamo gde ne treba da se pojavi Archive Link mogao da se doda prazan post

--- content-aside.php
+++ content-aside.php
@@ -1,8 +1,6 @@
 <article class="article-container aside" id="post-<?php the_ID();?>">
 	<!-- mobile -->
-	<div class="top-block-mobile titre-gris">
-		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-	</div>
+	
 	<div class="<?php if ( has_post_thumbnail() ) { ?>mobile-thumbnail <?php } ?>">
 		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small-mobile'); ?></a>
 	</div>
@@ -11,22 +9,25 @@
 		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small'); ?></a>
 	</div>
 	<div class="post-container">
-		<div class="top-block titre-gris">
-			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><!-- post titre -->
-		</div>
-
+		
 		<!-- content -->
-		<p class="post-excerpt">
+		<div class="post-excerpt">
 		<?php if ($post->post_excerpt) { ?>
+			<div class="titre-gris">
+				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+			</div>
+			<p>
 			<?php echo get_the_excerpt(); ?>
 			<br><a class="savoir" href="<?php the_permalink(); ?>">
 			<?php _e( 'En savoir +' , 'ifserbie' );  ?></a>
+			</p>
 		<?php } else {
 			the_content();
 		} ?>
-		</p>
+		</div>
 
 	</div>
 </article>
+
 
 
